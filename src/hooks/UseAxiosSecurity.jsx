7759304bcb2d1d6e977bb71a87bbc0d16bb67ee2
alihasna32 <muktermukter555@gmail.com>
2025-11-13 00:43:57 +0600

    router setup and custom hook fully done

---
+++ src/hooks/UseAxiosSecurity.jsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { use, useEffect } from "react";
+import { useNavigate } from "react-router";
+import { AuthContext } from "../context/AuthContext";
+
+const instance = axios.create({
+  baseURL: "http://localhost:3000",
+});
+const UseAxiosSecure = () => {
+  const { user, signOutUser } = use(AuthContext)
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const requestInterceptors = instance.interceptors.request.use((config) => {
+      const token = user.accessToken;
+      if (token) {
+        config.headers.authorization = `Bearer ${token}`;
+      }
+      return config;
+    });
+
+    const responseInterceptors = instance.interceptors.response.use(
+      (res) => {
+        return res;
+      },
+      (err) => {
+        const status = err.status;
+        if (status === 401 || status === 403) {
+          signOutUser().then(() => {
+            navigate("/registration");
+          });
+        }
+      }
+    );
+
+    return () => {
+      instance.interceptors.request.eject(requestInterceptors);
+      instance.interceptors.response.eject(responseInterceptors);
+    };
+  }, [user, instance, signOutUser, navigate]);
+  return instance;
+};
+export default UseAxiosSecure;
---
+++ src/routes/routers.jsx
@@ -0,0 +1,76 @@
+import { createBrowserRouter } from "react-router";
+import HomeLayout from "../layout/HomeLayout";
+import Login from "../pages/Login";
+import Registration from "../pages/Registration";
+import AddAJobs from "../pages/AddAJobs";
+import AllJobs from "../pages/AllJobs";
+import JobDetails from "./../components/jobDetails";
+import Update from "../components/Update";
+import MyAcceptedTasks from "../pages/MyAcceptedTasks";
+import HomePage from "../pages/HomePage";
+import error404 from "../assets/error-404.png";
+import MyAddedJobs from "../pages/MyAddedJobs";
+import PrivateRoutes from "../context/PrivateRoutes";
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout></HomeLayout>,
+    children: [
+      {
+        path: "/login",
+        element: <Login></Login>,
+      },
+      {
+        path: "/registration",
+        element: <Registration></Registration>,
+      },
+      {
+        path: "/",
+        element: <HomePage></HomePage>
+      },
+      {
+        path: "/addJob",
+        element: <PrivateRoutes>
+          <AddAJobs></AddAJobs>
+        </PrivateRoutes>,
+      },
+      {
+        path: "/allJobs",
+        element: <AllJobs></AllJobs>,
+      },
+      {
+        path: "/jobDetails/:id",
+        element: <PrivateRoutes>
+          <JobDetails></JobDetails>
+        </PrivateRoutes>,
+      },
+      {
+        path: "/updateJob/:id",
+        element: <Update></Update>,
+      },
+      {
+        path: "/myAcceptedTasks",
+        element: <PrivateRoutes>
+          <MyAcceptedTasks></MyAcceptedTasks>
+        </PrivateRoutes>,
+      },
+      {
+        path: "/myAddedJobs",
+        element: <PrivateRoutes>
+          <MyAddedJobs></MyAddedJobs>
+        </PrivateRoutes>,
+      },
+      {
+        path: "/*",
+        element: (
+          <div className="flex flex-col justify-center items-center min-h-screen gap-5">
+            <img className="w-[400px]" src={error404} alt="404 Not Found" />
+            <h1 className="text-6xl font-black">Page not found</h1>
+          </div>
+        ),
+      }
+    ],
+  },
+]);
+
+export default router;
